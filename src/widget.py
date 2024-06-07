f41d6236fa8b66f738f4f6e81d4123f4b8dcaabb
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-06-07 16:01:15 +0430

    add mask account card func

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,14 @@
-import masks
+from masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(account_card: str) -> str:
     """Функция, которая маскирует номер карты или счета"""
+    masked_number = ""
+    for symbols in account_card:
+        if symbols.isalpha():
+            masked_number += symbols + " "
+        elif symbols.isdigit() and len(symbols) == 16:
+            masked_number += get_mask_card_number(symbols)
+        elif symbols.isdigit() and len(symbols) == 18:
+            masked_number += get_mask_account(symbols)
+    return masked_number
