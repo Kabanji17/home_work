736d2f145dce8f718444657d9dcd819f4d3f1748
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-06-07 16:18:52 +0430

    add get data func and tests

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,20 @@
-from masks import get_mask_card_number, get_mask_account
+from .masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(account_card: str) -> str:
     """Функция, которая маскирует номер карты или счета"""
     masked_number = ""
-    for symbols in account_card:
-        if symbols.isalpha():
-            masked_number += symbols + " "
-        elif symbols.isdigit() and len(symbols) == 16:
-            masked_number += get_mask_card_number(symbols)
-        elif symbols.isdigit() and len(symbols) == 18:
-            masked_number += get_mask_account(symbols)
+    for symbol in account_card:
+        if symbol.isalpha():
+            masked_number += symbol
+    for symbol in account_card:
+        if symbol.isdigit() and len(symbol) == 16:
+            masked_number += get_mask_card_number(symbol)
+        elif symbol.isdigit() and len(symbol) == 18:
+            masked_number += get_mask_account(symbol)
     return masked_number
+
+
+def get_data(data: str) -> str:
+    """Функция преобразования даты формата ГГГГ-ММ-ДД в формат ДД-ММ-ГГГГ"""
+    return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
