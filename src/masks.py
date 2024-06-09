5528c19f46184d4d6373aee27667ef90e6f85276
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-06-09 14:12:21 +0430

    add fixes to masks

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,6 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номер карты"""
-    return f"{card_number[:5]} {card_number[5:7]}{"*" * 2} {"*" * 4} {card_number[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
