027ccab6054f3256e3493fcbef25f5b874deb39c
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-06-15 18:58:00 +0430

    add func sort by date

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,16 @@
-origin_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-               ]
+from typing import Any
 
 
-def filter_by_state(origin_list: list[dict[str, str | int]], state: str='EXECUTED') -> list[dict[str, str | int]]:
-    """ Функция, сортирующая список по введенному ключу"""
+origin_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(origin_list: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
+    """Функция, сортирующая список по введенному ключу"""
     filtered_list = []
     for data in origin_list:
         if data.get("state") == state:
@@ -14,4 +18,7 @@ def filter_by_state(origin_list: list[dict[str, str | int]], state: str='EXECUTE
     return filtered_list
 
 
-def
+def sort_by_date(origin_list: list[dict[str, Any]], reverse_list: bool = True) -> list[dict[str, Any]]:
+    """Функция, сортирующия по дате в порядке возрастания или убывания"""
+    sorted_list = sorted(origin_list, key=lambda d: d.get("date"), reverse=reverse_list)
+    return sorted_list
