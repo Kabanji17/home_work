2e926c829527be881b704332280dfbe75af6445e
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-07-14 23:38:53 +0430

    add fixes to decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,27 +1,30 @@
-from typing import Any, Callable
 from functools import wraps
+from typing import Any, Callable
 
 
-def log(filename=None) -> Callable:
+def log(filename: Any = None) -> Callable:
     """Запись вызова функции и ее результат в файл или в консоль"""
+
     def decorator(func: Callable) -> Callable:
+        @wraps(func)
         def wrapper(*args: Any, **kwargs: Any):
             try:
                 result = func(*args, **kwargs)
                 if filename:
-                    with open(filename, "a") as log_file:
+                    with open(filename, "a", encoding="utf-8") as log_file:
                         log_file.write(f"{func.__name__} ok")
                 else:
                     print(f"{func.__name__} ok")
                 return result
             except Exception as e:
                 if filename:
-                    with open(filename, "a") as log_file:
+                    with open(filename, "a", encoding="utf-8") as log_file:
                         log_file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
                 else:
                     print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
-                raise
+
         return wrapper
+
     return decorator
 
 
@@ -29,4 +32,5 @@ def log(filename=None) -> Callable:
 def my_function(x, y):
     return x + y
 
-my_function(1, 2)
+
+#my_function("1", 2)
