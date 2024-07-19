233c5b4252996d5ac640355ed3c8a7cc0e9e265c
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-07-19 13:45:27 +0430

    add logging to utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
+import logging
 import json
+
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("../logs/utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def get_transaction_info(filename: str) -> list[dict]:
@@ -7,8 +16,15 @@ def get_transaction_info(filename: str) -> list[dict]:
     try:
         with open(filename, "r", encoding="utf-8") as open_file:
             data_info = json.load(open_file)
+            logger.info("Файл успешно открыт")
             if type(data_info) is not list:
+                logger.error("Ошибка: TypeError")
                 return []
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        logger.error(f"Ошибка: {ex}")
         return []
     return data_info
+
+
+if __name__ == '__main__':
+    print(get_transaction_info(filename='../data/operations.json'))
